d349289af877eca974b6eeb4b52670fd7e4f6b35
rjdavis13 <rjdavis13@gmail.com>
2016-02-17 20:14:44 -0800

    all the files for learn python the hard way

---
+++ ex10.py
@@ -0,0 +1,17 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm Split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+print "\\ 1\' 2\" 3\a 4\b 5\f 6\n 7\r 8\t test \v test"
