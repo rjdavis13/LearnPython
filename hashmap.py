2a17cb1c8b1b4ec0afd2af3778789c8c1a0cd0fe
rjdavis13 <rjdavis13@gmail.com>
2016-03-09 22:56:21 -0800

    fix comments add ex40

--- hashmap.py
+++ hashmap.py
@@ -1,12 +1,12 @@
 def new(num_buckets=256):
-	"""Initializes a Map with athe given number of buckets."""
+	"""Initializes a Map with the given number of buckets."""
 	aMap = []
 	for i in range(0, num_buckets):
 		aMap.append([])
 	return aMap
 	
 def hash_key(aMap, key):
-	"""Given a key this will creat a number and then convert it to
+	"""Given a key this will create a number and then convert it to
 	an index for the aMap's buckets."""
 	return hash(key) % len(aMap)
 	
