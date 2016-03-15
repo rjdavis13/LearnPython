db7d4b0b3f43b6f0820f8490b836db30a6d438c0
rjdavis13 <rjdavis13@gmail.com>
2016-03-15 16:41:25 -0700

    added the python to english tester the non copy version doesnt work for indentation error reasons I cannot explain

--- oop_test.py
+++ oop_test.py
@@ -8,9 +8,9 @@ WORDS = []
 PHRASES = {
 	"class %%%(%%%):":
 		"Make a class named %%% that is-a %%%.",
-	"class %%%(object):/n/tdef __init__(self, ***)":
+	"class %%%(object):\n\tdef __init__(self, ***)" :
 		"class %%% has-a __init__ that takes self and *** parameters.",
-	"class %%%(object):/n/tdef ***(sefl, @@@)":
+	"class %%%(object):\n\tdef ***(self, @@@)":
 		"class %%% has-a function named *** that takes self and @@@ parameters.",
 	"*** = %%%()":
 		"Set *** to an instance of class %%%.",
@@ -53,9 +53,13 @@ def convert(snippet, phrase):
 		for word in other_names:
 			result = result.replace("***", word, 1)
 		
+        # fake parameter lists
+        for word in param_names:
+			result = result.replace("@@@", word, 1)
+		
 		results.append(result)
 		
-	return result
+	return results
 	
 	
 # keep going until they CTRL-D
@@ -66,17 +70,14 @@ try:
 		
 		for snippet in snippets:
 			phrase = PHRASES[snippet]
-			random.shuffle(snippets)
-			
-			for snippet in snippets:
-				phrase = PHRASES[snippet]
-				question, answer = convert(snippet, phrase)
-				if PHRASE_FIRST:
-					question, answer = answer, question
+
+			question, answer = convert(snippet, phrase)
+			if PHRASE_FIRST:
+				question, answer = answer, question
+		
+			print question
 				
-				print question
-				
-				raw_input("> ")
-				print "ANSWER:  %s/n/n" % answer
+			raw_input("> ")
+			print "ANSWER:  %s\n\n" % answer
 except EOFError:
-	print "/nBye"
+	print "\nBye"
